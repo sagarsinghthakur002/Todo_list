520e51bd683db140b8e27a5730a08b6146980ea7
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-09-08 17:29:04 +0545

    feat:  todo-list

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="container">
+      <div className="bg-red-600">hello</div>
+      <div className="bg-blue-600">world</div>
+      </div>
+
+  </>
+  )
+}
+
+export default App
