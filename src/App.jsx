543d460fdb2db170a6c4f0e10fa24e7b95be6fa5
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-09-11 21:58:33 +0545

    feat: adding text

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,18 @@ function App() {
   return (
     <>
     <Navbar/>
+      <div className='container mx-auto my-5 rounded-xl p-5 bg-vilolet-100'>
+        <div className='addTodo'>
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type='text' />
+          <button >Add</button>
+        </div>
+      <h2 classname='text-lg font-bold '> Your Task </h2>
+      <div className='todo'>
+        <div className='todoItem'>
+          
 
+      </div>
 
       
    </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nva className='felx justify-between bg-slate'>
+    <nva className='felx justify-between bg-slate-700 text-white py-2'>
       <div className='logo'>
         <span className='font-blod text-xl mx-8'> 
           my tastk 
@@ -10,8 +10,8 @@ const Navbar = () => {
       </div>
         
         <ul className='flex gap-9 '>
-        <li className='cursor-pointer hover:font-bold'>Home</li>
-        <li className='cursor-pointer hover:font-bold'>your Task</li>          
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>your Task</li>          
         </ul>
     </nav>
     
