22cdfb95653bd8547e9281553e7b90597c0a933d
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-09-12 21:24:39 +0545

    feat : some change

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,30 @@ import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
- 
+
   return (
     <>
-    <Navbar/>
-      <div className='container mx-auto my-5 rounded-xl p-5 bg-vilolet-100'>
+      <Navbar />
+      <div className='container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-full'>
         <div className='addTodo'>
           <h2 className='text-lg font-bold'>Add a Todo</h2>
           <input type='text' />
-          <button >Add</button>
+          <button>Add</button>
         </div>
-      <h2 classname='text-lg font-bold '> Your Task </h2>
-      <div className='todo'>
-        <div className='todoItem'>
-          
-
+        <h2 className='text-lg font-bold'> Your Task </h2>
+        <div className='todo'>
+          <div className='todo flex'>
+            <div className="text"> 
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus cupiditate a dolor enim, est aliquam! Praesentium quibusdam, cumque facere nostrum non laborum, aliquam eos sunt, repellendus quas vitae! Except
+            </div>
+            <div className="button">
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
       </div>
-
-      
-   </>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nva className='felx justify-between bg-slate-700 text-white py-2'>
+    <nav className='flex justify-between bg-slate-700 text-white py-2'>
       <div className='logo'>
-        <span className='font-blod text-xl mx-8'> 
-          my tastk 
+        <span className='font-bold text-xl mx-8'> 
+          my task
         </span>
       </div>
         
-        <ul className='flex gap-9 '>
+      <ul className='flex gap-9'>
         <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-        <li className='cursor-pointer hover:font-bold transition-all'>your Task</li>          
-        </ul>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Task</li>          
+      </ul>
     </nav>
-    
-      
-   
   )
 }
 
-export default Navbar
+export default Navbar;
